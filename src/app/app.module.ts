34972d73f62dceb4a78c6aa9f3f221c8dbdaa2a1
gatotoadrian <gatotoadrian@gmail.com>
2022-04-12 00:35:45 +0300

    modified file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteFromComponent } from './quote-from/quote-from.component';
 import { QouteDetailsComponent } from './app/qoute-details/qoute-details.component';
 import { QouteComponent } from './app/qoute/qoute.component';
+import { QoComponent } from './qo/qo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFromComponent,
     QouteDetailsComponent,
-    QouteComponent
+    QouteComponent,
+    QoComponent
   ],
   imports: [
     BrowserModule,
