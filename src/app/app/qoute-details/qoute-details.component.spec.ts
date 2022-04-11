646f598a512a3789feb3017cf35947e21420a41f
gatotoadrian <gatotoadrian@gmail.com>
2022-04-12 00:38:08 +0300

    modified file

--- src/app/app/qoute-details/qoute-details.component.spec.ts
+++ src/app/app/qoute-details/qoute-details.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QouteDetailsComponent } from './qoute-details.component';
+import { QuoteDetailComponent } from '././qoute-details.component';
 
-describe('QouteDetailsComponent', () => {
-  let component: QouteDetailsComponent;
-  let fixture: ComponentFixture<QouteDetailsComponent>;
+describe('QuoteDetailComponent', () => {
+  let component: QuoteDetailComponent;
+  let fixture: ComponentFixture<QuoteDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QouteDetailsComponent ]
+      declarations: [ QuoteDetailComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(QouteDetailsComponent);
+    fixture = TestBed.createComponent(QuoteDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
