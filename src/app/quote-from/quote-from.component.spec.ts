9f2a04e6e15269c670dee4b094e61170ac89b2dd
gatotoadrian <gatotoadrian@gmail.com>
2022-04-12 00:44:14 +0300

    modified file

--- src/app/quote-from/quote-from.component.spec.ts
+++ src/app/quote-from/quote-from.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QuoteFormComponent } from './quote-form.component';
+import { QuoteFormComponent } from './quote-from.component';
 
 describe('QuoteFormComponent', () => {
   let component: QuoteFormComponent;
